9ca172cd50ebb1e16e7be62eab5343ce3e98a6c4
Johannes Maria Frank <jmfrank63@gmail.com>
2023-02-16 15:39:49 +0000

    Macros working

--- macros/src/lib.rs
+++ macros/src/lib.rs
@@ -10,19 +10,37 @@ enum Json {
     Object(HashMap<String, Json>),
 }
 
+// ([ ??? ]) => {
+//     Json::Array(???)
+// };
+// ({ ??? }) => {
+//     Json::Object(
+//         ???
+//     )
+// };
+// (???) => {
+//     ??? // Number, String, Boolean
+// };
+
 macro_rules! json {
     (null) => { Json::Null };
-    // ([ ??? ]) => {
-    //     Json::Array(???)
-    // };
-    // ({ ??? }) => {
-    //     Json::Object(
-    //         ???
-    //     )
-    // };
-    // (???) => {
-    //     ??? // Number, String, Boolean
-    // };
+    // Parses an array of strings into a Json::Array
+    ([$($value:tt),*]) => {
+        Json::Array(vec![$(json!($value)),*])
+    };
+
+    ({ $($key:tt: $value:tt),* }) => {
+        {
+            let mut map = HashMap::new();
+            $(map.insert(String::from($key), json!($value));)*
+            Json::Object(map)
+        }
+    };
+    ($value:expr) => {
+        Json::from($value)
+     };
+     
+
 }
 
 #[cfg(test)]
@@ -34,83 +52,193 @@ mod tests {
         assert_eq!(json!(null), Json::Null);
     }
 
-    // #[test]
-    // fn parse_a_valid_boolean() {
-    //     assert_eq!(json!(true), Json::Boolean(true));
-    // }
+    #[test]
+    fn parse_a_valid_boolean() {
+        assert_eq!(json!(true), Json::Boolean(true));
+    }
 
-    // #[test]
-    // fn parse_a_valid_string() {
-    //     assert_eq!(json!("Hello"), Json::String(String::from("Hello")));
-    // }
+    #[test]
+    fn parse_a_valid_string() {
+        assert_eq!(json!("Hello"), Json::String(String::from("Hello")));
+    }
 
-    // #[test]
-    // fn parse_a_valid_array() {
-    //     assert_eq!(
-    //         json!(["a", "b", "c"]),
-    //         Json::Array(vec![
-    //             Json::String(String::from("a")),
-    //             Json::String(String::from("b")),
-    //             Json::String(String::from("c")),
-    //         ])
-    //     );
-    // }
+    #[test]
+    fn parse_a_valid_array() {
+        assert_eq!(
+            json!(["a", "b", "c"]),
+            Json::Array(vec![
+                Json::String(String::from("a")),
+                Json::String(String::from("b")),
+                Json::String(String::from("c")),
+            ])
+        );
+    }
 
-    // #[test]
-    // fn parse_a_valid_array_of_arrays() {
-    //     assert_eq!(
-    //         json!([["a", "b"], ["c"]]),
-    //         Json::Array(vec![
-    //             Json::Array(vec![
-    //                 Json::String(String::from("a")),
-    //                 Json::String(String::from("b"))
-    //             ]),
-    //             Json::Array(vec![Json::String(String::from("c"))]),
-    //         ])
-    //     );
-    // }
+    #[test]
+    fn parse_a_valid_array_of_arrays() {
+        assert_eq!(
+            json!([["a", "b"], ["c"]]),
+            Json::Array(vec![
+                Json::Array(vec![
+                    Json::String(String::from("a")),
+                    Json::String(String::from("b"))
+                ]),
+                Json::Array(vec![Json::String(String::from("c"))]),
+            ])
+        );
+    }
 
-    // #[test]
-    // fn parse_a_valid_object() {
-    //     assert_eq!(json!({
-    //         "Hello": "world",
-    //         "Test": 1,
-    //         "Names": [
-    //             "John",
-    //             "Doe"
-    //         ]
-    //     }), Json::Object(
-    //         vec![
-    //             (String::from("Hello"), Json::String(String::from("world"))),
-    //             (String::from("Test"), Json::Number(1.0)),
-    //             (String::from("Names"), Json::Array(vec![
-    //                 Json::String(String::from("John")),
-    //                 Json::String(String::from("Doe")),
-    //             ])),
-    //         ].into_iter().collect()
-    //     ));
-    // }
+    #[test]
+    fn parse_a_valid_object() {
+        assert_eq!(
+            json!({
+                "Hello": "world",
+                "Test": 1,
+                "Names": [
+                    "John",
+                    "Doe"
+                ]
+            }),
+            Json::Object(
+                vec![
+                    (String::from("Hello"), Json::String(String::from("world"))),
+                    (String::from("Test"), Json::Number(1.0)),
+                    (
+                        String::from("Names"),
+                        Json::Array(vec![
+                            Json::String(String::from("John")),
+                            Json::String(String::from("Doe")),
+                        ])
+                    ),
+                ]
+                .into_iter()
+                .collect()
+            )
+        );
+    }
 
-    // #[test]
-    // fn parse_a_valid_number() {
-    //     assert_eq!(json!(1), Json::Number(1.0));
-    //     assert_eq!(json!(1u8), Json::Number(1.0));
-    //     assert_eq!(json!(1u16), Json::Number(1.0));
-    //     assert_eq!(json!(1u32), Json::Number(1.0));
-    //     assert_eq!(json!(1u64), Json::Number(1.0));
-    //     assert_eq!(json!(1u128), Json::Number(1.0));
-    //     assert_eq!(json!(1usize), Json::Number(1.0));
-    //     assert_eq!(json!(1i8), Json::Number(1.0));
-    //     assert_eq!(json!(1i16), Json::Number(1.0));
-    //     assert_eq!(json!(1i32), Json::Number(1.0));
-    //     assert_eq!(json!(1i64), Json::Number(1.0));
-    //     assert_eq!(json!(1i128), Json::Number(1.0));
-    //     assert_eq!(json!(1isize), Json::Number(1.0));
-    // }
+    #[test]
+    fn parse_a_valid_number() {
+        assert_eq!(json!(1), Json::Number(1.0));
+        assert_eq!(json!(1u8), Json::Number(1.0));
+        assert_eq!(json!(1u16), Json::Number(1.0));
+        assert_eq!(json!(1u32), Json::Number(1.0));
+        assert_eq!(json!(1u64), Json::Number(1.0));
+        assert_eq!(json!(1u128), Json::Number(1.0));
+        assert_eq!(json!(1usize), Json::Number(1.0));
+        assert_eq!(json!(1i8), Json::Number(1.0));
+        assert_eq!(json!(1i16), Json::Number(1.0));
+        assert_eq!(json!(1i32), Json::Number(1.0));
+        assert_eq!(json!(1i64), Json::Number(1.0));
+        assert_eq!(json!(1i128), Json::Number(1.0));
+        assert_eq!(json!(1isize), Json::Number(1.0));
+    }
 
-    // #[test]
-    // fn parse_a_valid_float() {
-    //     assert_eq!(json!(1.0f32), Json::Number(1.0));
-    //     assert_eq!(json!(1.0f64), Json::Number(1.0));
-    // }
+    #[test]
+    fn parse_a_valid_float() {
+        assert_eq!(json!(1.0f32), Json::Number(1.0));
+        assert_eq!(json!(1.0f64), Json::Number(1.0));
+    }
+}
+
+impl From<bool> for Json {
+    fn from(value: bool) -> Self {
+        Json::Boolean(value)
+    }
+}
+
+impl From<String> for Json {
+    fn from(value: String) -> Self {
+        Json::String(value)
+    }
+}
+
+impl From<&str> for Json {
+    fn from(value: &str) -> Self {
+        Json::String(String::from(value))
+    }
+}
+
+impl From<f64> for Json {
+    fn from(value: f64) -> Self {
+        Json::Number(value)
+    }
+}
+
+impl From<f32> for Json {
+    fn from(value: f32) -> Self {
+        Json::Number(value as f64)
+    }
+}
+
+impl From<u8> for Json {
+    fn from(value: u8) -> Self {
+        Json::Number(value as f64)
+    }
+}
+
+impl From<u16> for Json {
+    fn from(value: u16) -> Self {
+        Json::Number(value as f64)
+    }
+}
+
+impl From<u32> for Json {
+    fn from(value: u32) -> Self {
+        Json::Number(value as f64)
+    }
+}
+
+impl From<u64> for Json {
+    fn from(value: u64) -> Self {
+        Json::Number(value as f64)
+    }
+}
+
+impl From<u128> for Json {
+    fn from(value: u128) -> Self {
+        Json::Number(value as f64)
+    }
+}
+
+impl From<usize> for Json {
+    fn from(value: usize) -> Self {
+        Json::Number(value as f64)
+    }
+}
+
+impl From<i8> for Json {
+    fn from(value: i8) -> Self {
+        Json::Number(value as f64)
+    }
+}
+
+impl From<i16> for Json {
+    fn from(value: i16) -> Self {
+        Json::Number(value as f64)
+    }
+}
+
+impl From<i32> for Json {
+    fn from(value: i32) -> Self {
+        Json::Number(value as f64)
+    }
+}
+
+impl From<i64> for Json {
+    fn from(value: i64) -> Self {
+        Json::Number(value as f64)
+    }
+}
+
+impl From<i128> for Json {
+    fn from(value: i128) -> Self {
+        Json::Number(value as f64)
+    }
+}
+
+impl From<isize> for Json {
+    fn from(value: isize) -> Self {
+        Json::Number(value as f64)
+    }
 }
--- threads/src/main.rs
+++ threads/src/main.rs
@@ -1,15 +1,36 @@
+use std::sync::mpsc::Sender;
 use std::thread;
 use std::time::Duration;
 
-struct ThreadPool {}
+struct ThreadPool {
+    handles: Vec<thread::JoinHandle<()>>,
+    sender: Sender<Box<dyn FnOnce() + Send + Sync + 'static>>,
+}
 
-impl ThreadPool {
+impl ThreadPool
+{
     fn new(number_of_threads: u8) -> Self {
-        todo!()
+        let (sender, receiver) = std::sync::mpsc::channel::<Box<dyn FnOnce() + Send + Sync + 'static>>();
+        let mut handles = vec![];
+        for _ in 0..number_of_threads {
+            handles.push(thread::spawn(|| {
+                while let Ok(task) = receiver.recv() {
+                    sender.send(task).unwrap();
+                }
+            }));
+        }
+        ThreadPool { handles, sender }
     }
 
-    fn execute(&self, task: ?) {
-        todo!()
+    fn execute(&self, task: impl FnOnce() + Send + Sync + 'static) {
+        self.sender.send(Box::new(task)).unwrap();
+    }
+
+    fn stop(&self) {
+        drop(&self.sender);
+        for handle in &self.handles {
+            handle.join().unwrap();
+        }
     }
 }
 
@@ -20,6 +41,7 @@ fn main() {
         thread::sleep(Duration::from_secs(2));
         println!("SLOW Hello from thread");
     });
+
     for i in 0..15 {
         pool.execute(move || {
             println!("FAST Hello from thread for task: {}", i);
@@ -29,5 +51,5 @@ fn main() {
     // First we're making sure enough time is given to threads to execute the tasks
     // Then, replace this line with the `stop` method.
     thread::sleep(Duration::from_secs(3));
-    // pool.stop();
+    pool.stop();
 }
